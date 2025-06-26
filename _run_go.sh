5fab34e9fff27a9e0df0c84858b184887a55c38e
Cheol-Will <kangcs@naver.com>
2025-06-26 00:23:45 +0000

    Backup

--- _run_go.sh
+++ _run_go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 arch_type="moe-mlp"
-data_list=("california" "churn" "covtype2" "diamond" "higgs-small" "house" "microsoft")
+data_list=("california" "churn" "covtype2" "diamond" "higgs-small" "house" "microsoft" "adult" "black-friday")
 # data_list=("adult" "black-friday" "california" "churn" "covtype2" "diamond" "higgs-small" "house" "microsoft" "otto" "tabred" "why")
 data="adult"
 
