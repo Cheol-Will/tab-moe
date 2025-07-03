c3b85ce3ca33b41c130420663b30658075170566
Cheol-Will <kangcs@naver.com>
2025-07-03 15:15:57 +0000

    Add rank logic

--- summary_metrics.py
+++ summary_metrics.py
@@ -35,8 +35,6 @@ def summary_metrics_table(model_list, data_list, output_path="output/metrics.csv
     """
     Aggregate performance scores from multiple models into a single table and save as CSV.
     """
-
-
     all_results = {}
 
     for model in model_list:
@@ -60,7 +58,8 @@ def summary_metrics_table(model_list, data_list, output_path="output/metrics.csv
     if is_save:
         df_all.to_csv(output_path, float_format="%.4f")
         print(f"Saved all model metrics to {output_path}")
-
+    
+    return df_all
 
 def summary_hyperparameters_one_model(model, is_print = False, is_save = False):
 
@@ -114,6 +113,7 @@ def summary_hyperparameters_one_model(model, is_print = False, is_save = False):
 
     if is_save:
         dfh.to_csv(f"exp/{model}/hyperparameters.csv")        
+
 
 def summary_hyperparameters(model_list, output_path="output/average_hyperparameters.csv", is_print = False, is_save = False):
     """
@@ -207,6 +207,63 @@ def print_tuning_time(model):
     df = df.loc[idx, "time"]
     df.to_csv(f"seraching_time_{model}.csv")
 
+def save_rank_csv(model: str, data_list: list[str]) -> pd.DataFrame:
+    paper = pd.read_csv("output/paper_metrics.csv")
+    df = summary_metrics_table([model], data_list, is_print=False, is_save=False)
+    model_cols = [c for c in paper.columns if c not in ("dataset", "direction")]
+
+    rank_df = pd.DataFrame(index=paper["dataset"], columns=model_cols, dtype=float)
+    n_models = len(model_cols)
+    for i, row in paper.iterrows():
+        ds = row["dataset"]
+        asc = (row["direction"] == "lower_is_better")  # True if regression else False
+        # 해당 행의 모델 값만 Series로 뽑아 랭크
+        ranks = row[model_cols].rank(ascending=asc, method="min")
+        # filNaN 
+        ranks = ranks.fillna(n_models)
+        rank_df.loc[ds] = ranks
+    avg_rank = rank_df.mean(axis=0)
+    print("\nRank in paper_metrics")
+    print(avg_rank.sort_values())
+    print()
+
+
+    summary_t = df.T.reset_index().rename(columns={"Dataset": "dataset"})
+    merged = paper.merge(summary_t, on="dataset", how="left")
+    merged = merged[merged["dataset"].isin(data_list)]
+    merged.drop(columns=["TabPFN"], axis=1, inplace=True)
+
+
+    # preprocessing
+    merged.loc[merged["dataset"] == "house", model] /= 10000
+    model_cols = [c for c in merged.columns if c not in ("dataset", "direction")]
+    merged[model_cols] = merged[model_cols].abs()
+    print(merged)
+
+
+    rank_df = pd.DataFrame(index=merged["dataset"], columns=model_cols, dtype=float)
+
+    n_models = len(model_cols)
+    for i, row in merged.iterrows():
+        ds = row["dataset"]
+        asc = (row["direction"] == "lower_is_better")  # True if regression else False
+        # 해당 행의 모델 값만 Series로 뽑아 랭크
+        ranks = row[model_cols].rank(ascending=asc, method="min")
+        # filNaN 
+        ranks = ranks.fillna(n_models)
+        rank_df.loc[ds] = ranks
+
+    print(rank_df)
+    avg_rank = rank_df.mean(axis=0)
+    print(avg_rank.sort_values())
+    out_file = f"output/rank_{model}.csv"
+    merged.to_csv(out_file, index=False, float_format="%.4f")
+    print(f"\n▶ Saved merged ranking to `{out_file}`")
+    print(merged)
+    return merged
+
+
+
 def main():
     model_list = [
         'mlp-piecewiselinear',
@@ -224,12 +281,23 @@ def main():
         'tabrmv2-piecewiselinear', # Retrieval + TabM (Batch ensemble)
     ]
     data_list = [
-        "adult", "black-friday", "california", "churn", "covtype2", "diamond", 
-        "higgs-small", "house", "microsoft", "otto",  
+        "adult", 
+        "black-friday", 
+        "california", 
+        "churn", 
+        # "covtype2", 
+        "diamond", 
+        "higgs-small", 
+        "house", 
+        # "microsoft", 
+        "otto",  
     ] 
-
-    summary_metrics_table(model_list, data_list, output_path="output/metrics.csv", is_print=True, is_save=True)
-    summary_hyperparameters(model_list, output_path="output/average_hyperparameters.csv", is_print=False, is_save=True)
+    model_list = [
+        'tabrm-piecewiselinear'
+    ]
+    summary_metrics_table(model_list, data_list, output_path="output/metrics.csv", is_print=True, is_save=False)
+    summary_hyperparameters(model_list, output_path="output/average_hyperparameters.csv", is_print=False, is_save=False)
+    save_rank_csv('tabrm-piecewiselinear', data_list)
 
 if __name__ == "__main__":
     main()
